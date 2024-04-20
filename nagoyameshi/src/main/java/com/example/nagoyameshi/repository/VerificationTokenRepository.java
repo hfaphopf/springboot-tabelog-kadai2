d148c3381cfdb651696e366deb567488ac033e53
hfaphopf <kuma.shou0925@gmail.com>
2024-04-20 21:39:06 +0900

    Update VerificationTokenRepository.java

--- nagoyameshi/src/main/java/com/example/nagoyameshi/repository/VerificationTokenRepository.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/repository/VerificationTokenRepository.java
@@ -6,4 +6,6 @@ import com.example.nagoyameshi.entity.VerificationToken;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
 	public VerificationToken findByToken(String token);
+	
+	public VerificationToken findByUserId(Integer UserId);
 }
