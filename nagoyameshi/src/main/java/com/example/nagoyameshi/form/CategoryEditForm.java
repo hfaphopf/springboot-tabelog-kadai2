76961b11729ec7978135de15f72de468e5455bdc
hfaphopf <kuma.shou0925@gmail.com>
2024-04-29 08:07:57 +0900

    Create CategoryEditForm.java

---
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/form/CategoryEditForm.java
@@ -0,0 +1,16 @@
+package com.example.nagoyameshi.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CategoryEditForm {
+	@NotNull
+	private Integer id;
+	
+	@NotBlank(message = "カテゴリ名を入力してください。")
+	private String name;
+}
