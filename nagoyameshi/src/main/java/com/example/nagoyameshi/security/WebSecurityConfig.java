afff85993e33a76989f2e4c45b922b6390a0eb19
hfaphopf <kuma.shou0925@gmail.com>
2024-05-07 17:15:51 +0900

    Update WebSecurityConfig.java

--- nagoyameshi/src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
@@ -19,8 +19,7 @@ public class WebSecurityConfig {
 		http
 				.authorizeHttpRequests((requests) -> requests
 						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**",
-								"/restaurants", "/restaurants/{id}", "/reset/**")
-						.permitAll() // すべてのユーザーにアクセスを許可するURL 
+								"/restaurants", "/restaurants/{id}", "/reset/**", "/error/**", "/error").permitAll() // すべてのユーザーにアクセスを許可するURL 
 						.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者にのみアクセスを許可するURL
 						.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
 				)
