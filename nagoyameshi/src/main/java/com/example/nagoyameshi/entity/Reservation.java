3856fc98077f9762e1bca0085055c2ea8aa4db20
hfaphopf <kuma.shou0925@gmail.com>
2024-05-10 17:30:17 +0900

    Update Reservation.java

--- nagoyameshi/src/main/java/com/example/nagoyameshi/entity/Reservation.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/entity/Reservation.java
@@ -1,113 +1,48 @@
-package com.example.nagoyameshi.controller;
+package com.example.nagoyameshi.entity;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
-import com.example.nagoyameshi.entity.Reservation;
-import com.example.nagoyameshi.entity.Restaurant;
-import com.example.nagoyameshi.entity.User;
-import com.example.nagoyameshi.form.ReservationInputForm;
-import com.example.nagoyameshi.form.ReservationRegisterForm;
-import com.example.nagoyameshi.repository.ReservationRepository;
-import com.example.nagoyameshi.repository.RestaurantRepository;
-import com.example.nagoyameshi.security.UserDetailsImpl;
-import com.example.nagoyameshi.service.ReservationService;
+@Entity
+@Table(name = "reservations")
+@Data
+public class Reservation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
 
-@Controller
-public class ReservationController {
-	private final ReservationRepository reservationRepository;
-	private final RestaurantRepository restaurantRepository;
-	private final ReservationService reservationService;
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id")
+	private Restaurant restaurant;
 
-	public ReservationController(ReservationRepository reservationRepository, RestaurantRepository restaurantRepository,
-			ReservationService reservationService) {
-		this.reservationRepository = reservationRepository;
-		this.restaurantRepository = restaurantRepository;
-		this.reservationService = reservationService;
-	}
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
-	@GetMapping("/reservations")
-	public String index(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
-			Model model) {
-		User user = userDetailsImpl.getUser();
-		Page<Reservation> reservationPage = reservationRepository.findByUserOrderByCreatedAtDesc(user, pageable);
+	@Column(name = "number_of_people")
+	private Integer numberOfPeople;
 
-		model.addAttribute("reservationPage", reservationPage);
+	@Column(name = "reservation_date")
+	private LocalDate reservationDate;
+	
+	@Column(name = "reservation_time")
+    private LocalTime reservationTime;
 
-		return "reservations/index";
-	}
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private Timestamp createdAt;
 
-	@GetMapping("/restaurants/{id}/reservations/input")
-	public String confirm(@PathVariable(name = "id") Integer id,
-			@ModelAttribute ReservationInputForm reservationInputForm,
-			BindingResult bindingResult,
-			RedirectAttributes redirectAttributes,
-			@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
-			Model model) {
-		Restaurant restaurant = restaurantRepository.getReferenceById(id);
-		Integer numberOfPeople = reservationInputForm.getNumberOfPeople();
-		Integer seats = restaurant.getSeats();
-
-		if (numberOfPeople != null) {
-			if (!reservationService.isWithinSeats(numberOfPeople, seats)) {
-				FieldError fieldError = new FieldError(bindingResult.getObjectName(), "numberOfPeople",
-						"人数が定員を超えています。");
-				bindingResult.addError(fieldError);
-			}
-		}
-
-		if (bindingResult.hasErrors()) {
-			model.addAttribute("restaurant", restaurant);
-			model.addAttribute("errorMessage", "予約内容に不備があります。");
-			return "restaurants/show";
-		}
-
-		redirectAttributes.addFlashAttribute("reservationInputForm", reservationInputForm);
-
-		return "redirect:/restaurants/{id}/reservations/confirm";
-	}
-
-	@GetMapping("/restaurants/{id}/reservations/confirm")
-	public String confirm(@PathVariable(name = "id") Integer id,
-			@ModelAttribute ReservationInputForm reservationInputForm,
-			@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
-			Model model) {
-		Restaurant restaurant = restaurantRepository.getReferenceById(id);
-		User user = userDetailsImpl.getUser();
-
-		//日付を表示する
-		LocalDate checkinDate = reservationInputForm.getCheckinDate();
-		LocalTime checkinTime = reservationInputForm.getCheckinTime();
-		ReservationRegisterForm reservationRegisterForm = new ReservationRegisterForm(restaurant.getId(), user.getId(),
-				checkinDate.toString(), checkinTime.toString(),reservationInputForm.getNumberOfPeople());
-
-		model.addAttribute("restaurant", restaurant);
-		model.addAttribute("reservationRegisterForm", reservationRegisterForm);
-
-		return "reservations/confirm";
-	}
-
-	// フォームの送信先を担当するメソッド
-	@PostMapping("/restaurants/{id}/reservations/create")
-	public String create(@ModelAttribute ReservationRegisterForm reservationRegisterForm) {
-		reservationService.create(reservationRegisterForm);
-
-		return "redirect:/reservations?reserved";
-	}
+	@Column(name = "updated_at", insertable = false, updatable = false)
+	private Timestamp updatedAt;
 }
