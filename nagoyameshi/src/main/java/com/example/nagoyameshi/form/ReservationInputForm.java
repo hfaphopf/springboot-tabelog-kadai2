8651f69b9089621f5be8eb61c84dc940c44e3a3b
hfaphopf <kuma.shou0925@gmail.com>
2024-05-10 17:29:29 +0900

    Update ReservationInputForm.java

--- nagoyameshi/src/main/java/com/example/nagoyameshi/form/ReservationInputForm.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/form/ReservationInputForm.java
@@ -1,6 +1,7 @@
 package com.example.nagoyameshi.form;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -12,6 +13,9 @@ public class ReservationInputForm {
 	@NotBlank(message = "予約日を選択してください。")
 	private String reservationDate;
 
+	@NotBlank(message = "予約時間を選択してください。")
+    private String reservationTime;
+	
 	@NotNull(message = "人数を入力してください。")
 	@Min(value = 1, message = "人数は1人以上に設定してください。")
 	private Integer numberOfPeople;
@@ -32,4 +36,21 @@ public class ReservationInputForm {
 	public void setCheckinDate(LocalDate checkinDate) {
 		this.checkinDate = checkinDate;
 	}
+	
+
+	// 予約時間を取得する
+	//フィールド
+		private LocalTime checkinTime;
+
+		//ゲッター
+		public LocalTime getCheckinTime() {
+			//        String checkReservationTime = getReservationTime();
+			//        return LocalTime.parse(checkReservationTime);
+			return this.checkinTime;
+		}
+
+		//セッター
+		public void setCheckinTime(LocalTime checkinTime) {
+			this.checkinTime = checkinTime;
+		}
 }
